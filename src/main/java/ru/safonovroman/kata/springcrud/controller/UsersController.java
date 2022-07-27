21cbbc003f511e1a9f5aa76f8cc92130c81d902e
Roman <safonov.roman.86@yandex.ru>
2022-07-27 14:01:50 +1000

    Second try

--- src/main/java/ru/safonovroman/kata/springcrud/controller/UsersController.java
+++ src/main/java/ru/safonovroman/kata/springcrud/controller/UsersController.java
@@ -11,59 +11,52 @@ import ru.safonovroman.kata.springcrud.service.UserService;
 @Controller
 public class UsersController {
 
-	@Autowired
-	UserService userService;
+    @Autowired
+    UserService userService;
 
-	@GetMapping(value = "/users")
-	public String showAllUsers(ModelMap model) {
-		model.addAttribute("users", userService.listUsers());
-		return "users";
-	}
+    @GetMapping(value = "/users")
+    public String showAllUsers(ModelMap model) {
+        model.addAttribute("users", userService.listUsers());
+        return "users";
+    }
 
-	@GetMapping(value = "/users/new")
-	public String showCreateUser(ModelMap model) {
-		User user = new User();
-		model.addAttribute("user", user);
-		return "user";
-	}
+    @GetMapping(value = "/users/new")
+    public String showCreateUser(ModelMap model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "user";
+    }
 
-	@PostMapping(value = "/users/new")
-	public RedirectView createUser(ModelMap model,
-							 @RequestParam("email") String email,
-							 @RequestParam("firstName") String firstName,
-							 @RequestParam("lastName") String lastName) {
-		User user = new User();
-		user.setEmail(email);
-		user.setFirstName(firstName);
-		user.setLastName(lastName);
-		userService.add(user);
-		return new RedirectView("/users");
-	}
+    @PostMapping(value = "/users/new")
+    public RedirectView createUser(ModelMap model, @ModelAttribute("user") User user) {
+        userService.add(user);
+        return new RedirectView("/users");
+    }
 
-	@GetMapping(value = "/user/{id}")
-	public String showUser(ModelMap model, @PathVariable("id") Long id) {
-		model.addAttribute("user", userService.getUser(id));
-		return "user";
-	}
+    @GetMapping(value = "/user/{id}")
+    public String showUser(ModelMap model, @PathVariable("id") Long id) {
+        model.addAttribute("user", userService.getUser(id));
+        return "user";
+    }
 
-	@PostMapping(value = "/user/{id}")
-	public String updateUser(ModelMap model,
-							 @PathVariable("id") Long id,
-							 @RequestParam("email") String email,
-							 @RequestParam("firstName") String firstName,
-							 @RequestParam("lastName") String lastName) {
-		User user =  userService.getUser(id);
-		user.setEmail(email);
-		user.setFirstName(firstName);
-		user.setLastName(lastName);
-		userService.update(user);
-		model.addAttribute("user", user);
-		return "user";
-	}
+    @PostMapping(value = "/user/{id}")
+    public String updateUser(ModelMap model,
+                             @PathVariable("id") Long id,
+                             @RequestParam("email") String email,
+                             @RequestParam("firstName") String firstName,
+                             @RequestParam("lastName") String lastName) {
+        User user = userService.getUser(id);
+        user.setEmail(email);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        userService.update(user);
+        model.addAttribute("user", user);
+        return "user";
+    }
 
-	@GetMapping(value = "/user/{id}/delete")
-	public RedirectView deleteUser(ModelMap model, @PathVariable("id") Long id) {
-		userService.delete(id);
-		return new RedirectView("/users");
-	}
+    @GetMapping(value = "/user/{id}/delete")
+    public RedirectView deleteUser(ModelMap model, @PathVariable("id") Long id) {
+        userService.delete(id);
+        return new RedirectView("/users");
+    }
 }
